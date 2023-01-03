b7b1d86bba93133467a7546ee615c4aecd7c8cc0
toubal77 <72252709+toubal77@users.noreply.github.com>
2023-01-03 17:15:13 +0100

    init class elect

--- component_node/elect.py
+++ component_node/elect.py
@@ -1,5 +1,33 @@
+from uuid import uuid4
+
+leader_id = 0
+leader_port = 0
+
 class Message:
     def __init__(self, id_elect, port_elect):
         self.id_elect = id_elect
         self.port_elect = port_elect
         self.type = "ELECT"
+
+class Elect:
+    def __init__(self, port_in):
+        global leader_port, leader_id
+        self.id = uuid4()
+        #shoould have value of the next noeud
+        leader_id = self.id
+        leader_port = port_in
+
+    def sur_reception_de(self,M):
+        global leader_id, leader_port
+        if M.id_elect > leader_id :
+            leader_id = M.id_elect
+            leader_port = M.port_elect
+        else:
+            M.id_elect = leader_id
+            M.port_elect = leader_port
+        self.envoyer_a(M)
+
+    def envoyer_a(self, M):
+        M=M
+        #bahratli, normalement khasna attribue next_node bsh f projet
+        #basah f les instructions ta3 projet marahach dayratah
